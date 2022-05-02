bca91edef14995de0576f88d6f335e592db1d0b7
Nghia Le <davidle314@hotmail.com>
2022-05-01 22:53:40 -0700

    install need file

---
+++ server.js
@@ -0,0 +1,8 @@
+// import mysql
+const mysql = require('mysql');
+// import inquirer
+const inquire = require('inquirer');
+// import console.table
+const consoleTable = require('console.table')
+
+
