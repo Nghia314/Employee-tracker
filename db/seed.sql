fab2a2467587313e7c631e68483790a05ff3cddd
Nghia Le <davidle314@hotmail.com>
2022-05-02 20:39:52 -0700

    finish with sql

--- db/seed.sql
+++ db/seed.sql
@@ -15,3 +15,10 @@ VALUES
 ('software Engineer', 300000, 3),
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Devin','John', 1, null),
+('John','Miller', 1, 3),
+('Smith','John', 2, 3),
+('Ana','Gomez',5, 4),
+('Tyler', 'Smith', 4, null),
+('David','John', 6, null),
