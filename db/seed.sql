73c2f4c042542ca36505c8dafb0e47d48474f586
Nghia Le <davidle314@hotmail.com>
2022-05-02 20:29:35 -0700

    adjusting the file

--- db/seed.sql
+++ db/seed.sql
@@ -8,3 +8,10 @@ VALUES
 
 INSERT INTO role (title, salary, department_id)
 VALUES
+('Full stack web developer', 100000, 1),
+('Accountant', 20000, 2),
+('financial manager', 300000, 2),
+('accounting manager', 300000, 3),
+('software Engineer', 300000, 3),
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
